c0ee334e996973c2645d9ac7786eadfbefe67064
Sebastinez <sebastinez@me.com>
2019-08-31 20:11:15 -0300

    (feat) Se agregaron los includes de php y los parciales (nav, header y footer)

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>E-Commerce</title>
+</head>
+
+<body>
+    <?php
+    include("./includes/nav.html");
+    ?>
+
+    <?php
+    include("./includes/header.html");
+    ?>
+
+    <!-- Acá debe ir el contenido cambiante entre header y footer -->
+
+    <?php
+    include("./includes/footer.html");
+    ?>
+</body>
+
+</html>
