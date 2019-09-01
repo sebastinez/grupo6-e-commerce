13d90ec6f56cec894e01aa8e3aff64c2d8c695aa
Eze Casa <elzeke55@gmail.com>
2019-09-01 13:13:00 -0300

    (feat) Se agrego el header #13 y nav #11

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Ubuntu:400,400i,500,500i,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
+    <link rel="stylesheet" href="css/animated.css">
     <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="./css/style.css">
     <title>E-Commerce</title>
