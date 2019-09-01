58244c81b4a8c685c464a13cf2d06fd4a7803c14
Sebastinez <sebastinez@me.com>
2019-09-01 00:09:45 -0300

    (fix) Cambio de clase div principal de container por container-fluid

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>E-Commerce</title>
 </head>
 
 <body>
-    <div class="container">
+    <div class="container-fluid">
         <?php
         include("./includes/nav.html");
         ?>
