6262cf96b77a2af7ed17c3de56e9ff2cdaa64e7c
Ezequiel <stuartsezequiel@gmail.com>
2019-09-03 00:09:38 -0300

    (feat) Cambio de logo, y se agrego favicon

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="icon" type="image/ico" href="img/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Permanent+Marker|Ubuntu:400,400i,500,500i,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css">
