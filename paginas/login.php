c5f17a955d565d2bf8ebbd6057d96c10ea010ff2
Sebastinez <sebastinez@me.com>
2019-09-05 13:36:35 -0300

    (feat) Agrega pagina login #7, y otras mejoras pequeñas

--- paginas/login.php
+++ paginas/login.php
@@ -1,2 +1,29 @@
 <?php include("./includes/breadcrumb.php"); ?>
-<h1 class="titulo-body">Login</h1>
+<div class="login">
+
+    <?php if (!$_POST) { ?>
+
+        <div class="header">
+            <h2>¡Hola! Para seguir, <br>ingresá tu e-mail o usuario</h2>
+        </div>
+        <form class="body" action="?p=login" method="POST">
+            <div class="form-group">
+                <input type="text" name="usuario" class="form-control usuario" placeholder="e-mail o usuario">
+            </div>
+            <button type="submit" class="btn btn-primary">Ingresar</button>
+        </form>
+
+    <?php } elseif (isset($_POST["usuario"])) { ?>
+
+        <div class="header">
+            <h2>Ahora, <br>tu contraseña</h2>
+        </div>
+        <form class="body" action="paginas/auth.php" method="POST">
+            <div class="form-group">
+                <input type="hidden" name="usuario" value="<?= $_POST["usuario"] ?>">
+                <input type="password" name="pwd" class="form-control usuario" placeholder="e-mail o usuario">
+            </div>
+            <button type="submit" class="btn btn-primary">Ingresar</button>
+        </form>
+
+    <?php } ?>
