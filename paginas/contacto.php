a3b8c86019212a32ab31127ab7441694b2b7b1c8
Federico <Bacino>
2019-09-05 16:36:10 -0300

    (feat) Implementación de formulario de contacto #8
    Lo hice flotante por si despues queremos hacerlo popup

--- paginas/contacto.php
+++ paginas/contacto.php
@@ -1,2 +1,17 @@
 <?php include("./includes/breadcrumb.php"); ?>
 <h1 class="titulo-body">Formulario de contacto</h1>
+<div class="container">
+    <div id="main-form">
+        <div id="form-div">
+            <form class="form" id="" >
+            <p class="name">
+                <input name="name" type="text" class="input" required placeholder="Name" id="name" />
+            </p>
+            <p class="email">
+                <input name="email" type="email" required class="input" id="email" placeholder="Email" />
+            </p>
+            <p class="text">
+                <textarea name="message" class="input" id="comment" placeholder="Message"></textarea>
+            </p>
+        <div class="submit">
+            <button type="submit" class="btn btn-default">SUBMIT</button>
