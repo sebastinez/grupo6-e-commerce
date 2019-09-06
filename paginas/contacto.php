bac304d07f9bf8c9fd2d032a8ffac1e94ee4bf7f
Sebastinez <sebastinez@me.com>
2019-09-06 11:11:05 -0300

    (fix) Cambios al formulario de contacto #8
    Para hacerlo estilo bootstrap y mas responsive

--- paginas/contacto.php
+++ paginas/contacto.php
@@ -1,22 +1,23 @@
 <?php include("./includes/breadcrumb.php"); ?>
 <h1 class="titulo-body">Formulario de contacto</h1>
-<div class="container">
+<div class="contacto">
     <div class="form-group">
-        <div id="form-div">
-            <form class="form" id="">
-                <p class="name">
-                    <input name="name" type="text" class="input" required placeholder="Name" id="name" />
-                </p>
-                <p class="email">
-                    <input name="email" type="email" required class="input" id="email" placeholder="Email" />
-                </p>
-                <p class="text">
-                    <textarea name="message" class="input" id="comment" placeholder="Message"></textarea>
-                </p>
-                <div class="submit">
-                    <button type="submit" class="btn btn-default">SUBMIT</button>
-                </div>
-            </form>
-        </div>
+        <form class="form" id="">
+            <div class="form-group">
+                <label for="name">Nombre</label>
+                <input name="name" type="text" class="form-control input" required placeholder="Nombre" id="name" />
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input name="email" type="email" required class="form-control form-control input" id="email" placeholder="Email" />
+            </div>
+            <div class="form-group">
+                <label for="comment">Mensaje</label>
+
+                <textarea name="message" rows="5" class="form-control input" id="comment" placeholder="Mensaje"></textarea>
+            </div>
+            <div class="submit">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
     </div>
 </div>
