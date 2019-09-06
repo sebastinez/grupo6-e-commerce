4598bdcefc0f37379747cf5f5b208c30c5df3ef9
Ezequiel <stuartsezequiel@gmail.com>
2019-09-06 01:03:00 -0300

    (feat) Se agrego la pagina home #4 y la pagina que contiene los discos #3 y se acomodaron colores

---
+++ paginas/albums.php
@@ -0,0 +1,88 @@
+<div class="container pt-5">
+	<div class="container-albums">
+		
+		<div class="contenedor-card-album">
+			<div class="card-album">
+				<img src="img/thumbs/1.jpg" alt="">
+			</div>
+			<div class="info">
+				<a href="#"><p class="title">Titulo de album</p></a>
+				<p class="name">Descripcion</p>
+				<span class="precio">$2000.-</span>
+				<span><span class="btn btn-naranja">comprar</span></span>
+			</div>
+		</div>
+
+		<div class="contenedor-card-album">
+			<div class="card-album">
+				<img src="img/thumbs/2.jpg" alt="">
+			</div>
+			<div class="info">
+				<a href="#"><p class="title">Titulo de album</p></a>
+				<p class="name">Descripcion</p>
+				<span class="precio">$2000.-</span>
+				<span><span class="btn btn-naranja">comprar</span></span>
+			</div>
+		</div>
+
+		<div class="contenedor-card-album">
+			<div class="card-album">
+				<img src="img/thumbs/3.jpg" alt="">
+			</div>
+			<div class="info">
+				<a href="#"><p class="title">Titulo de album</p></a>
+				<p class="name">Descripcion</p>
+				<span class="precio">$2000.-</span>
+				<span><span class="btn btn-naranja">comprar</span></span>
+			</div>
+		</div>
+
+		<div class="contenedor-card-album">
+			<div class="card-album">
+				<img src="img/thumbs/4.jpg" alt="">
+			</div>
+			<div class="info">
+				<a href="#"><p class="title">Titulo de album</p></a>
+				<p class="name">Descripcion</p>
+				<span class="precio">$2000.-</span>
+				<span><span class="btn btn-naranja">comprar</span></span>
+			</div>
+		</div>
+
+		<div class="contenedor-card-album">
+			<div class="card-album">
+				<img src="img/thumbs/5.jpg" alt="">
+			</div>
+			<div class="info">
+				<a href="#"><p class="title">Titulo de album</p></a>
+				<p class="name">Descripcion</p>
+				<span class="precio">$2000.-</span>
+				<span><span class="btn btn-naranja">comprar</span></span>
+			</div>
+		</div>
+
+		<div class="contenedor-card-album">
+			<div class="card-album">
+				<img src="img/thumbs/6.jpg" alt="">
+			</div>
+			<div class="info">
+				<a href="#"><p class="title">Titulo de album</p></a>
+				<p class="name">Descripcion</p>
+				<span class="precio">$2000.-</span>
+				<span><span class="btn btn-naranja">comprar</span></span>
+			</div>
+		</div>
+
+		<div class="contenedor-card-album">
+			<div class="card-album">
+				<img src="img/thumbs/7.jpg" alt="">
+			</div>
+			<div class="info">
+				<a href="#"><p class="title">Titulo de album</p></a>
+				<p class="name">Descripcion</p>
+				<span class="precio">$2000.-</span>
+				<span><span class="btn btn-naranja">comprar</span></span>
+			</div>
+		</div>
+	</div>
+</div>
--- paginas/home.php
+++ paginas/home.php
@@ -1,2 +1,122 @@
 <?php include("./includes/breadcrumb.php"); ?>
-<h1 class="titulo-body">Home</h1>
+
+<div class="container pt-5">
+	<div class="contenedor-cards ">
+		<a href="?p=albums">
+			<div class="card-estilo">
+				<img src="img/pop.png" alt="">
+			<p>Pop</p>
+			</div>
+		</a>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/rock.png" alt="">
+			<p>Rock</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/hip-hop.png" alt="">
+			<p>hip-hop</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/trending.png" alt="">
+			<p>trending</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/latino.png" alt="">
+			<p>latino</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/latino.png" alt="">
+			<p>latino</p>
+		</div>
+	</div>
+</div>
+
+<div class="container container-scroll-home">
+	<div class="contenedor-cards ">
+		<div class="card-estilo">
+			<img src="img/pop.png" alt="">
+			<p>Pop</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/rock.png" alt="">
+			<p>Rock</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/hip-hop.png" alt="">
+			<p>hip-hop</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/trending.png" alt="">
+			<p>trending</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/latino.png" alt="">
+			<p>latino</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/latino.png" alt="">
+			<p>latino</p>
+		</div>
+	</div>
+</div>
+
+<div class="container container-scroll-home">
+	<div class="contenedor-cards ">
+		<div class="card-estilo">
+			<img src="img/pop.png" alt="">
+			<p>Pop</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/rock.png" alt="">
+			<p>Rock</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/hip-hop.png" alt="">
+			<p>hip-hop</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/trending.png" alt="">
+			<p>trending</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/latino.png" alt="">
+			<p>latino</p>
+		</div>
+	</div>
+	<div class="contenedor-cards">
+		<div class="card-estilo">
+			<img src="img/latino.png" alt="">
+			<p>latino</p>
+		</div>
+	</div>
+</div>
+
+<div class="padding-bottom-provisorio"></div>
