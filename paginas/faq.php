5cab5edda6b869b2ef06e701901c73a67cf05a0c
Federico <Bacino>
2019-09-06 17:20:15 -0300

    (feat) Implementación de FAQ #5

--- paginas/faq.php
+++ paginas/faq.php
@@ -1,2 +1,153 @@
 <?php include("./includes/breadcrumb.php"); ?>
 <h1 class="titulo-body">FAQ</h1>
+<section class=faq>
+		<div class="container" id="accordion">
+			<div class="faqHeader">Generales</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Como me registro?</a>
+					</h4>
+				</div>
+				<div id="collapseOne" class="panel-collapse collapse in">
+					<div class="card-block">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nisi doloribus ex dolorum quas laudantium illo fugiat aperiam, deserunt esse. Excepturi, iste nam? Eum nihil sit deleniti quasi quisquam est.
+					</div>
+				</div>
+			</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTen">Estan buenos los discos?</a>
+					</h4>
+				</div>
+				<div id="collapseTen" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos facilis quos fuga, molestiae quia itaque, provident molestias eius dolorum quae fugit reiciendis neque? Nostrum fugit rem modi alias laboriosam quasi!
+					</div>
+				</div>
+			</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEleven">Bla Bla</a>
+					</h4>
+				</div>
+				<div id="collapseEleven" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio iste pariatur, nostrum maxime aliquid enim deleniti esse libero velit magnam assumenda. Praesentium cupiditate ullam sunt eius id dicta laboriosam alias?
+					</div>
+				</div>
+			</div>
+	
+			<div class="faqHeader">Sarasa 1</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Bla bla</a>
+					</h4>
+				</div>
+				<div id="collapseTwo" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, fugiat eligendi. Dicta eum non quibusdam possimus totam ipsam, repudiandae veniam sed sint fugit tempore nihil, odit nulla animi iusto laudantium!
+					</div>
+				</div>
+			</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Bla bla</a>
+					</h4>
+				</div>
+				<div id="collapseThree" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis dicta rerum quas tempore. Perspiciatis quasi, vitae ullam fugiat earum magnam quas nemo esse, omnis quibusdam vero veniam qui numquam illum!
+						<ul>
+							<li>viri viri</li>
+							<li>viri viri</li>
+							<li>viri viri</li>
+							<li>viri viri</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFive">Bla bla
+						</a>
+					</h4>
+				</div>
+				<div id="collapseFive" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, accusamus, libero illum placeat dolore autem maxime distinctio eos doloremque cupiditate vitae iste, quia quidem cum nemo quod eligendi. Nostrum, eaque.
+						<br />
+					</div>
+				</div>
+			</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSix">bla bla</a>
+					</h4>
+				</div>
+				<div id="collapseSix" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa alias rerum quibusdam esse aperiam possimus hic optio corrupti nisi. Quisquam dicta odio libero iste. Temporibus aliquam optio perspiciatis sint rerum!
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseEight">bla bla</a>
+					</h4>
+				</div>
+				<div id="collapseEight" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe error, enim molestias ea mollitia excepturi ut, voluptates aliquid expedita consectetur aliquam delectus! Facilis dignissimos temporibus alias culpa ut et quam.
+					</div>
+				</div>
+			</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseNine">bla bla</a>
+					</h4>
+				</div>
+				<div id="collapseNine" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere, excepturi recusandae! Veritatis inventore, ex ullam cupiditate obcaecati modi praesentium corporis quas iure labore? Animi deleniti vero, inventore distinctio doloribus architecto?
+					</div>
+				</div>
+			</div>
+	
+			<div class="faqHeader">Sarasa 2</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">bla bla</a>
+					</h4>
+				</div>
+				<div id="collapseFour" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis nihil animi aliquam necessitatibus, cumque adipisci provident eligendi voluptate, et debitis dignissimos? Voluptates a at consequatur non eius quo! Voluptatum, aliquid?
+					</div>
+				</div>
+			</div>
+			<div class="card ">
+				<div class="card-header">
+					<h4 class="card-header">
+						<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseSeven">bla bla</a>
+					</h4>
+				</div>
+				<div id="collapseSeven" class="panel-collapse collapse">
+					<div class="card-block">
+						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae aliquid ad alias obcaecati commodi, architecto vero explicabo optio quam nesciunt adipisci temporibus repellat, error sint nemo omnis. In, soluta? Animi?
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
