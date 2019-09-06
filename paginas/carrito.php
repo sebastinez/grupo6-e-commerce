2f908a7479ab408d20c15258fd385d8931e30d18
Sebastian <sebastinez@me.com>
2019-09-06 19:28:46 -0300

    (feat) Implementación de carrito de compras #1

--- paginas/carrito.php
+++ paginas/carrito.php
@@ -1,2 +1,42 @@
 <?php include("./includes/breadcrumb.php"); ?>
 <h1 class="titulo-body">Carrito de compra</h1>
+<div class="carrito">
+    <hr>
+    <div class="row">
+        <div class="col-lg-4 col-md-12"><img src="img/thumbs/3.jpg"></div>
+        <div class="col-lg-5 col-md-12">
+            <h2>Pink Floyd - The Wall</h2><span>2000,00 ARS</span>
+            <p class="text-success">En Stock</p>
+            <select name="cantidad" id="" class="form-control">
+                <option value="">Cantidad...</option>
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+                <option value="">4</option>
+                <option value="">5+</option>
+            </select>
+            <button class="btn btn-danger">Borrar</button>
+        </div>
+        <div class="col-lg-3 col-md-12 precio-desktop">2000,00 ARS</div>
+
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-lg-4 col-md-12"><img src="img/thumbs/2.jpg"></div>
+        <div class="col-lg-5 col-md-12">
+            <h2>Pink Floyd</h2><span>2000,00 ARS</span>
+            <p class="text-danger">Demora de 10 días</p>
+            <select name="cantidad" id="" class="form-control">
+                <option value="">Cantidad...</option>
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+                <option value="">4</option>
+                <option value="">5+</option>
+            </select>
+            <button class="btn btn-danger">Borrar</button>
+        </div>
+        <div class="col-lg-3 col-md-12 precio-desktop">2000,00 ARS</div>
+
+    </div>
+</div>
