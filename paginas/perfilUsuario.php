66ce983f009d4309dcffe87e6097088ce4449efc
Sebastinez <sebastinez@me.com>
2019-09-06 10:43:49 -0300

    (feat) Cierre de perfil de usuario #9

--- paginas/perfilUsuario.php
+++ paginas/perfilUsuario.php
@@ -1,8 +1,24 @@
 <?php include("./includes/breadcrumb.php"); ?>
-<div class="perfil">
+<div class="perfil container">
     <img src="img/avatar.png" class="avatar">
-    <h2>Ariel Villafañe</h2>
-    <div class="genres">
-        <div class="cuadros"></div>
+    <h2>Alejandro Villafañe</h2>
+    <h4>Genres preferidos</h4>
+    <div class="row">
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/rock.png"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/pop.png"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/latino.png"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/trending.png"></div>
+
+    </div>
+    <h4>Discos comprados</h4>
+    <div class="row">
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/thumbs/1.jpg"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/thumbs/2.jpg"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/thumbs/3.jpg"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/thumbs/4.jpg"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/thumbs/5.jpg"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/thumbs/6.jpg"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/thumbs/7.jpg"></div>
+        <div class="cuadro col-lg-3 col-md-6 col-xs-12"><img src="img/thumbs/8.jpg"></div>
     </div>
 </div>
