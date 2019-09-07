06702f8d7316d811611d63421d23aad1fe26ff27
Ezequiel <stuartsezequiel@gmail.com>
2019-09-07 04:16:21 -0300

    (fix) Cambio de estilos en varias paginas

--- paginas/home.php
+++ paginas/home.php
@@ -1,24 +1,9 @@
-<?php include("./includes/breadcrumb.php"); ?>
-
 <?php
+	include("./includes/breadcrumb.php");
+	require("./data/generos.php");
+; ?>
 
-$generos = [
-			["genero" => "Country", "img_genero" => "img/generos/country.jpg"],
-			["genero" => "Disco", "img_genero" => "img/generos/disco.jpg"],
-			["genero" => "Folk", "img_genero" => "img/generos/folk.jpg"],
-			["genero" => "Hip-Hop", "img_genero" => "img/generos/hip-hop.jpg"],
-			["genero" => "Latino", "img_genero" => "img/generos/latino.jpg"],
-			["genero" => "Pop", "img_genero" => "img/generos/pop.jpg"],
-			["genero" => "Reggae", "img_genero" => "img/generos/reggae.jpg"],
-			["genero" => "Rock", "img_genero" => "img/generos/rock.jpg"],
-			["genero" => "House", "img_genero" => "img/generos/house.jpg"],
-			["genero" => "Soul", "img_genero" => "img/generos/soul.jpg"],
-			["genero" => "Techno", "img_genero" => "img/generos/techno.jpg"],
-			["genero" => "jazz", "img_genero" => "img/generos/jazz.jpg"],
-			["genero" => "Metal", "img_genero" => "img/generos/metal.jpg"],
-			["genero" => "Blues", "img_genero" => "img/generos/blues.jpg"],
-		];
-?>
+
 
 
 <div class="container container-generos mt-5">
