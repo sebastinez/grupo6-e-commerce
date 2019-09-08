9bf2d5435864f02a138bd32175d4c947934e4f39
Ezequiel <stuartsezequiel@gmail.com>
2019-09-07 22:12:33 -0300

    (fix) Cambio de estilos en contacto y header

--- paginas/contacto.php
+++ paginas/contacto.php
@@ -4,15 +4,15 @@
     <div class="form-group">
         <form class="form" id="">
             <div class="form-group">
-                <label for="name">Nombre</label>
+                <!-- <label for="name">Nombre</label> -->
                 <input name="name" type="text" class="form-control input" required placeholder="Nombre" id="name" />
             </div>
             <div class="form-group">
-                <label for="email">Email</label>
+                <!-- <label for="email">Email</label> -->
                 <input name="email" type="email" required class="form-control form-control input" id="email" placeholder="Email" />
             </div>
             <div class="form-group">
-                <label for="comment">Mensaje</label>
+                <!-- <label for="comment">Mensaje</label> -->
 
                 <textarea name="message" rows="5" class="form-control input" id="comment" placeholder="Mensaje"></textarea>
             </div>
