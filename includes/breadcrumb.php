7f3dc8b14921d697d11e53aa8044be70262e2cab
Sebastinez <sebastinez@me.com>
2019-09-10 13:37:57 -0300

    (fix) Arreglo de #issue 19 por formato de imagenes
    Las imagenes que no son cuadradas llevaban a que el texto seguia desalineado..
    Y en resoluciónes menores se agrandaba todo.
    Resuelto con media queries y object-fit

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -1,8 +1,8 @@
 <div class="container">
-	<div class="bread">
-    <?php if ($p != "home") : ?>
-        <i class="fas fa-home fa-2x"></i> <span id="breadcrumb-home"><a href="index.php">Home</a> </span>
-        / <span id="breadcrumb-page"><?= ucwords($p); ?></span>
-    <?php endif; ?>
-</div>
+    <div class="bread">
+        <?php if ($p != "home") : ?>
+            <i class="fas fa-home fa-2x"></i> <span id="breadcrumb-home"><a href="index.php">Home</a> </span>
+            <span id="breadcrumb-barra"> / </span><span id="breadcrumb-page"><?= ucwords($p); ?></span>
+        <?php endif; ?>
+    </div>
 </div>
