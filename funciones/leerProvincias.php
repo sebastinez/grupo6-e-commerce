dfb07778e7ae5f1774c97168dbdee32c14c3aae8
Sebastian <sebastinez@me.com>
2019-09-18 23:46:37 -0300

    (feat) Implementacion de base de datos de ciudades y provincias

--- funciones/leerProvincias.php
+++ funciones/leerProvincias.php
@@ -1,12 +1,19 @@
 <?php
 
-$data = "../data/provincias.json";
+$data = "./data/ciudades.json";
 
 function obtenerProvincias()
 {
     $prov = [];
-    foreach (json_decode(file_get_contents($GLOBALS["data"]), true)["provincias"] as $provincia) {
-        $prov[] = $provincia["nombre"];
+    foreach (json_decode(file_get_contents($GLOBALS["data"]), true) as $provincia) {
+        $prov[] = ["nombre" => $provincia["nombre"], "id" => $provincia["id"]];
     }
     return $prov;
+}
+
+function obtenerCiudades($id)
+{
+    foreach (json_decode(file_get_contents($GLOBALS["data"]), true) as $provincia) {
+        if ($provincia["id"] == $id) return $provincia["ciudades"];
+    }
 }
