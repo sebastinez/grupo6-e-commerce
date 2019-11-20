613851fd456b55743f6e054791f2516ae192179b
Federico <Bacino>
2019-11-20 18:43:09 -0300

    (Fix) Ruta imagen Avatar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,9 +17,9 @@
     <div class="menu-item">
       <span>Hola {{Auth::user()->name}}</span>
       @if(Auth::user()->avatar == null)
-      <img src="img/avatar.png" data-toggle="dropdown" class="userAvatar">
-      @else 
-      <img src="storage/{{Auth::user()->avatar}}" data-toggle="dropdown" class="userAvatar">
+      <img src="/img/avatar.png" data-toggle="dropdown" class="userAvatar">
+      @else
+      <img src="/storage/{{Auth::user()->avatar}}" data-toggle="dropdown" class="userAvatar">
       @endif
       <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
         <a class="dropdown-item" href="/users/{{Auth::user()->id}}/edit">Editar perfil</a>
