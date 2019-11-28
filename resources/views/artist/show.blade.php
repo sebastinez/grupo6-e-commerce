d96707ccae8ea3ee79537dfe90a834b7fbdb7ffb
Ezequiel <stuartsezequiel@gmail.com>
2019-11-28 00:47:33 -0300

    fix de estilos

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -1,31 +1,41 @@
 @extends("index")
 
 @section("content")
-<div class="container pt-5">
-	<div class="titulos">{{$artist->name}}</div>
+<div class="container padding">
+	<div class="resultados">
+		<p>Resultados para: {{$artist->name}}</p>
+		</div>
 	<div class="container-albums">
 		@foreach ($albums as $album)
 
 		<div class="contenedor-card-album animated fadeIn faster">
-			<a href="/albums/{{$album->id}}">
+			
 				<div class="card-img">
-					<img src="{{$album->cover}}">
+					<a href="/albums/{{$album->id}}">
+						<img src="{{$album->cover}}">
+					</a>
 				</div>
 				<div class="info">
-					{{$album->artist[0]->name}}
-					<p class="name_disc">{{$album->name}}</p>
+					<p class="name_artist">{{$album->artist[0]->name}}</p>
+					
+					<p class="name_disc">
+						<a href="/albums/{{$album->id}}">{{$album->name}}</a>
+					</p>
 					<div class="info-precio">
+						<span class="">Total tracks {{$album->total_tracks}}</span>
 						<p class="anio">Lanzamiento {{$album->release_date}}</p>
 						<span class="precio">300 ARS</span>
 					</div>
 				</div>
-			</a>
+			
 			<button class="btn btn-naranja comprar">comprar</button>
 		</div>
 		@endforeach
 	</div>
 </div>
 
-{{ $albums->links() }}
+<div class="container pt-5 pb-5">
+		{{ $albums->links() }}
+</div>
 
 @endsection
