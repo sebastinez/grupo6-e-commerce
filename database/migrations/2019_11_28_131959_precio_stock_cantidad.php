0868661415e2eda327459b2bf9cb3a47da9ad24e
Sebastinez <sebastinez@me.com>
2019-11-28 13:47:34 -0300

    (fix) Nombre de migracion

--- database/migrations/2019_11_28_131959_precio_stock_cantidad.php
+++ database/migrations/2019_11_28_131959_precio_stock_cantidad.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class PrecioYStock extends Migration
+class PrecioStockCantidad extends Migration
 {
     /**
      * Run the migrations.
