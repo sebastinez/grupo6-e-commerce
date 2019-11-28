83d4cb5f87c8a7e7406dfe6a7c429d84952c7dd5
Sebastinez <sebastinez@me.com>
2019-11-28 14:36:03 -0300

    (fix) Agrega @auth a artist.show

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -27,12 +27,13 @@
 					<span class="precio">{{$album->precio}} ARS</span>
 				</div>
 			</div>
-
+			@auth
 			@if($album->stock > 0)
 			<button data-id="{{$album->id}}" data-type="comprar" data-user="{{Auth::user()->id}}" class="btn btn-naranja comprar">comprar</button>
 			@else
 			<button class="btn btn-secondary comprar" disabled>SIN STOCK</button>
 			@endif
+			@endauth
 		</div>
 		@endforeach
 	</div>
