e3571322d970d8b2a298984eae16556ce8d7a93b
Sebastinez <sebastinez@me.com>
2019-12-06 11:39:20 -0300

    (feat) Mejoras en carrito

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -17,8 +17,33 @@ class CartController extends Controller
      */
     public function index(Request $request)
     {
+
         $cart = Cart::with("album")->where("user_id", "=", $request->ip())->get();
         $albums = $cart[0]->getAlbumPaginatedAttribute();
-        return view("carrito.show", ["cart" => $cart[0], "albums" => $albums]);
+
+        $precioTotal = array_reduce($cart[0]->album->toArray(), function ($total, $item) {
+            $total += $item["precio"];
+            return $total;
+        });
+
+
+
+        $artistasEnCarrito = [];
+        foreach ($cart[0]->album as $album) {
+            if (!array_search($album->artist[0]->name, $artistasEnCarrito))
+                $artistasEnCarrito[] = $album->artist[0]->name;
+        }
+
+        $relacionados = Album::whereHas("artist", function ($q) use ($artistasEnCarrito) {
+            $q->whereIn("name", $artistasEnCarrito);
+        })->inRandomOrder()->limit(12)->get();
+
+
+        return view("carrito.show", [
+            "cart" => $cart[0],
+            "albums" => $albums,
+            "subtotal" => $precioTotal,
+            "relacionados" => $relacionados
+        ]);
     }
 };
--- resources/views/carrito/show.blade.php
+++ resources/views/carrito/show.blade.php
@@ -5,44 +5,81 @@
     <div class="container">
         {{ Breadcrumbs::render('cart') }}
         <div class="titulos">Carrito de compra</div>
-        @if(count($albums) > 0)
-        <div class="carrito">
-            @foreach ($albums as $album)
-            <div class="row pb-3" data-disco="d{{$album->id}}">
-                <div class="col-lg-4 col-md-12"><img src="{{$album->cover}}"></div>
-                <div class="col-lg-5 col-md-12">
-                    <h2>{{$album->name}}</h2><span>{{$album->precio}} ARS</span>
-                    <p>{{$album->artist[0]->name}}</p>
-                    @if($album->stock > 0)
-                    <p class="text-success">En Stock (quedan {{$album->stock}} discos)</p>
-                    @else
-                    <p class="text-warning">No hay stock</p>
-                    @endif
-                    <div class="form-row">
-                        <div class="col-3">
-                            <input type="number" name="cantidad" class="form-control" value="{{$album->pivot->cantidad}}" data-disco="d{{$album->id}}">
+        <div class="carrito-grid">
+            <div class="carrito-items">
+
+                @if(count($albums) > 0)
+                <div class="carritoActivo">
+                    @foreach ($albums as $album)
+                    <div class="row pb-3" data-disco="d{{$album->id}}">
+                        <div class="col-lg-4 col-md-12"><img src="{{$album->cover}}"></div>
+                        <div class="col-lg-5 col-md-12">
+                            <h2>{{$album->name}}</h2><span>{{$album->precio}} ARS</span>
+                            <p>{{$album->artist[0]->name}}</p>
+                            @if($album->stock > 0)
+                            <p class="text-success">En Stock (quedan {{$album->stock}} discos)</p>
+                            @else
+                            <p class="text-warning">No hay stock</p>
+                            @endif
+                            <div class="form-row">
+                                <div class="col-3">
+                                    <input type="number" name="cantidad" class="form-control" value="{{$album->pivot->cantidad}}" data-disco="d{{$album->id}}">
+                                </div>
+                                <div class="col-4">
+                                    <button class="btn btn-danger" data-stock="{{$album->stock}}" data-id="{{$album->id}}" data-type="update">Actualizar</button>
+                                </div>
+                                <div class="col-4">
+                                    <button class="btn btn-danger" data-id="{{$album->id}}" data-type="borrar">Borrar</button>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-4">
-                            <button class="btn btn-danger" data-stock="{{$album->stock}}" data-id="{{$album->id}}" data-type="update">Actualizar</button>
+                        <div class="col-lg-3 col-md-12 precio precio-desktop">{{$album->precio}} ARS</div>
+                    </div>
+                    @endforeach
+                </div>
+                @endif
+
+            </div>
+            <div class="carrito-subtotal">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="pb-3">
+                            <h3>Subtotal: {{$subtotal}} ARS</h3>
                         </div>
-                        <div class="col-4">
-                            <button class="btn btn-danger" data-id="{{$album->id}}" data-type="borrar">Borrar</button>
+                        <button class="btn btn-danger">Confirmar compra</button>
+                    </div>
+                </div>
+            </div>
+            <div class="carrito-relacionados">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="pb-3">
+                            <h3>Productos relacionados</h3>
+                            <div id="carrito-listadoRelacionados">
+                                @foreach($relacionados as $album)
+                                <a href="/albums/{{$album->id}}">
+                                    <img src="{{$album->cover}}" width="80">
+                                </a>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-12 precio precio-desktop">{{$album->precio}} ARS</div>
             </div>
-            @endforeach
         </div>
-        @endif
-        <div class="row paginado">
-	        {{$albums->links()}}
-        </div>
-        @unless(count($cart->album) > 0)
-        <div class="titulos" style="font-size:2rem">No hay discos en el carrito</div>
-        @endunless
     </div>
 </div>
 <script src="{{asset("/js/comprarDisco.js")}}"></script>
 
 @endsection
+
+
+
+
+<!--    <div class="row paginado">
+            {{$albums->links()}}
+        </div>
+        @unless(count($cart->album) > 0)
+        <div class="titulos" style="font-size:2rem">No hay discos en el carrito</div>
+        @endunless
+    -->
