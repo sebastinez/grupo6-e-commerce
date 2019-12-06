60b2ecd93b09d39bc02f3f75741747cf8875fe1a
Sebastinez <sebastinez@me.com>
2019-12-06 17:00:34 -0300

    (fix) correción de clases en querySelectors

--- public/js/comprarDisco.js
+++ public/js/comprarDisco.js
@@ -1,80 +1,80 @@
-var addBtn = document.querySelectorAll('button[data-type=comprar]');
-var updateBtn = document.querySelectorAll('button[data-type=update]');
-var borrarBtn = document.querySelectorAll('button[data-type=borrar]');
+var addBtn = document.querySelectorAll("button[data-type=comprar]");
+var updateBtn = document.querySelectorAll("button[data-type=update]");
+var borrarBtn = document.querySelectorAll("button[data-type=borrar]");
 
 addBtn.forEach(button => {
-    button.addEventListener('click', event => {
+    button.addEventListener("click", event => {
         event.preventDefault();
         let albumBtn = event.target;
 
         let form = new FormData();
-        form.append('album_id', albumBtn.getAttribute('data-id'));
-        form.append('cantidad', 1);
+        form.append("album_id", albumBtn.getAttribute("data-id"));
+        form.append("cantidad", 1);
 
-        fetch('/api/addDisco', {
+        fetch("/api/addDisco", {
             body: form,
-            method: 'POST'
+            method: "POST"
         }).then(response => {
             console.log(response);
 
             if (response.status == 200) {
-                albumBtn.innerHTML = 'LISTO!';
-                albumBtn.setAttribute('disabled', true);
-                albumBtn.classList.remove('btn-naranja');
-                albumBtn.classList.add('btn-secondary');
+                albumBtn.innerHTML = "LISTO!";
+                albumBtn.setAttribute("disabled", true);
+                albumBtn.classList.remove("btn-naranja");
+                albumBtn.classList.add("btn-secondary");
             } else {
-                albumBtn.innerHTML = 'ERROR!';
-                albumBtn.setAttribute('disabled', true);
-                albumBtn.classList.remove('btn-naranja');
-                albumBtn.classList.add('btn-danger');
+                albumBtn.innerHTML = "ERROR!";
+                albumBtn.setAttribute("disabled", true);
+                albumBtn.classList.remove("btn-naranja");
+                albumBtn.classList.add("btn-danger");
                 setTimeout(function() {
-                    albumBtn.innerHTML = 'COMPRAR';
-                    albumBtn.removeAttribute('disabled');
-                    albumBtn.classList.add('btn-naranja');
-                    albumBtn.classList.remove('btn-danger');
+                    albumBtn.innerHTML = "COMPRAR";
+                    albumBtn.removeAttribute("disabled");
+                    albumBtn.classList.add("btn-naranja");
+                    albumBtn.classList.remove("btn-danger");
                 }, 3000);
             }
         });
     });
 });
 updateBtn.forEach(button => {
-    button.addEventListener('click', event => {
+    button.addEventListener("click", event => {
         event.preventDefault();
         let albumBtn = event.target;
-        let id = albumBtn.getAttribute('data-id');
+        let id = albumBtn.getAttribute("data-id");
         let cantidad = document.querySelector(`input[data-disco=d${id}]`).value;
-        let stock = albumBtn.getAttribute('data-stock');
+        let stock = albumBtn.getAttribute("data-stock");
         if (cantidad > stock) {
-            albumBtn.innerHTML = 'No hay suficiente!';
-            albumBtn.setAttribute('disabled', true);
-            albumBtn.classList.remove('btn-danger');
-            albumBtn.classList.add('btn-secondary');
+            albumBtn.innerHTML = "No hay suficiente!";
+            albumBtn.setAttribute("disabled", true);
+            albumBtn.classList.remove("btn-danger");
+            albumBtn.classList.add("btn-secondary");
             setTimeout(function() {
-                albumBtn.innerHTML = 'Actualizar';
+                albumBtn.innerHTML = "Actualizar";
 
-                albumBtn.removeAttribute('disabled');
-                albumBtn.classList.add('btn-danger');
-                albumBtn.classList.remove('btn-secondary');
+                albumBtn.removeAttribute("disabled");
+                albumBtn.classList.add("btn-danger");
+                albumBtn.classList.remove("btn-secondary");
             }, 2000);
         } else {
             let form = new FormData();
-            form.append('album_id', albumBtn.getAttribute('data-id'));
-            form.append('cantidad', cantidad);
-            fetch('/api/updateDisco', { body: form, method: 'POST' }).then(
+            form.append("album_id", albumBtn.getAttribute("data-id"));
+            form.append("cantidad", cantidad);
+            fetch("/api/updateDisco", { body: form, method: "POST" }).then(
                 response => {
                     console.log(response);
 
                     if (response.status == 200) {
-                        albumBtn.innerHTML = 'LISTO!';
-                        albumBtn.setAttribute('disabled', true);
-                        albumBtn.classList.remove('btn-danger');
-                        albumBtn.classList.add('btn-secondary');
+                        albumBtn.innerHTML = "LISTO!";
+                        albumBtn.setAttribute("disabled", true);
+                        albumBtn.classList.remove("btn-danger");
+                        albumBtn.classList.add("btn-secondary");
                         setTimeout(function() {
-                            albumBtn.innerHTML = 'Actualizar';
+                            albumBtn.innerHTML = "Actualizar";
 
-                            albumBtn.removeAttribute('disabled');
-                            albumBtn.classList.add('btn-danger');
-                            albumBtn.classList.remove('btn-secondary');
+                            albumBtn.removeAttribute("disabled");
+                            albumBtn.classList.add("btn-danger");
+                            albumBtn.classList.remove("btn-secondary");
                         }, 2000);
                     }
                 }
@@ -83,28 +83,30 @@ updateBtn.forEach(button => {
     });
 });
 borrarBtn.forEach(button => {
-    button.addEventListener('click', event => {
+    button.addEventListener("click", event => {
         event.preventDefault();
         let albumBtn = event.target;
 
         let form = new FormData();
-        form.append('user_id', albumBtn.getAttribute('data-user'));
-        form.append('album_id', albumBtn.getAttribute('data-id'));
-        fetch('/api/destroyDisco', { body: form, method: 'POST' }).then(
+        form.append("user_id", albumBtn.getAttribute("data-user"));
+        form.append("album_id", albumBtn.getAttribute("data-id"));
+        fetch("/api/destroyDisco", { body: form, method: "POST" }).then(
             response => {
+                console.log(response);
+
                 if (response.status == 200) {
                     document
                         .querySelector(
                             `div[data-disco=d${albumBtn.getAttribute(
-                                'data-id'
+                                "data-id"
                             )}`
                         )
                         .remove();
                     if (
-                        document.querySelectorAll('div.carrito .row').length ===
-                        0
+                        document.querySelectorAll("div.carritoActivo .row")
+                            .length === 0
                     ) {
-                        document.querySelector('div.container').innerHTML +=
+                        document.querySelector("div.carritoActivo").innerHTML +=
                             "<div class='titulos' style='font-size:2rem'>No hay discos en el carrito</div>                    ";
                     }
                 }
