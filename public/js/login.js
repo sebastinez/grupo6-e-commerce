e4bf305611dbfd2d6a805dbe64c47de22de76013
Federico <Bacino>
2019-12-06 21:55:39 -0300

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,31 @@
-let email = document.querySelector('#login input[type=email]');
-let password = document.querySelector('#login input[type=password]');
-let token = document.querySelector('#login input[type=hidden]');
-let btn = document.querySelector('#login button[type=submit]');
-let formHTML = document.querySelector('#login form.body .form-group');
+let email = document.querySelector("#login input[type=email]");
+let password = document.querySelector("#login input[type=password]");
+let token = document.querySelector("#login input[type=hidden]");
+let btn = document.querySelector("#login button[type=submit]");
+let formHTML = document.querySelector("#login form.body .form-group");
 console.log(btn);
 
-btn.addEventListener('click', e => {
+btn.addEventListener("click", e => {
     e.preventDefault();
 
     let form = new FormData();
-    form.append('_token', token.value);
-    form.append('email', email.value);
-    form.append('password', password.value);
+    form.append("_token", token.value);
+    form.append("email", email.value);
+    form.append("password", password.value);
 
-    fetch('/api/checkUser', {
+    fetch("/api/checkUser", {
         body: form,
-        method: 'POST'
+        method: "POST"
     })
         .then(result => {
             if (result.status == 200) {
-                document.getElementById('login').submit();
+                document.getElementById("loginForm").submit();
             }
             if (result.status == 403) {
-                alert('No autorizado');
+                alert("No autorizado");
             }
         })
         .catch(error => {
-            console.error('Error:', error);
+            console.error("Error:", error);
         });
 });
